b320a57d92bd327d1c8a0201d781474eeec57bc8
st10357922 <st10357922@vcconnect.edu.za>
2025-10-21 12:10:50 +0200

    ICE TASK 3

---
+++ my-react-app/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Atlegang Mogatusi</h1>
+      <h2>Student Number: st10357922</h2>
+      <img
+        src="https://share.google/images/GeyVnUqAsMh9GvTfx"
+        alt="Atlegang Mogatusi"
+        style={{ borderRadius: '50%', marginTop: '20px' }}
+      />
+    </div>
+  );
+}
+
+export default App;
